8d8865d55a11d165e216736045bac62088897ab5
linmo111 <56517417+linmo111@users.noreply.github.com>
2023-01-30 06:43:08 +0800

    j

---
+++ task1/software/hello_world_sw/obj/default/DE_hello_world.d
@@ -0,0 +1,13 @@
+obj/default/DE_hello_world.o: DE_hello_world.c \
+ d:\infoprocess_workspace\labs\lab2\task1\software\hello_world_sw_bsp\hal\inc\sys\alt_stdio.h \
+ d:\infoprocess_workspace\labs\lab2\task1\software\hello_world_sw_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ d:\infoprocess_workspace\labs\lab2\task1\software\hello_world_sw_bsp\hal\inc\io.h \
+ d:\infoprocess_workspace\labs\lab2\task1\software\hello_world_sw_bsp\hal\inc\alt_types.h \
+ d:/InfoProcess_Workspace/Labs/Lab2/task1/software/hello_world_sw_bsp/system.h \
+ d:/InfoProcess_Workspace/Labs/Lab2/task1/software/hello_world_sw_bsp/linker.h
+d:\infoprocess_workspace\labs\lab2\task1\software\hello_world_sw_bsp\hal\inc\sys\alt_stdio.h:
+d:\infoprocess_workspace\labs\lab2\task1\software\hello_world_sw_bsp\drivers\inc\altera_avalon_pio_regs.h:
+d:\infoprocess_workspace\labs\lab2\task1\software\hello_world_sw_bsp\hal\inc\io.h:
+d:\infoprocess_workspace\labs\lab2\task1\software\hello_world_sw_bsp\hal\inc\alt_types.h:
+d:/InfoProcess_Workspace/Labs/Lab2/task1/software/hello_world_sw_bsp/system.h:
+d:/InfoProcess_Workspace/Labs/Lab2/task1/software/hello_world_sw_bsp/linker.h:
