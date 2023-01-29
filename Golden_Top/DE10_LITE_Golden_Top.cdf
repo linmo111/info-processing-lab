1102f189720c58f68dca8dced6f7a0ce26bfc467
linmo111 <56517417+linmo111@users.noreply.github.com>
2023-01-30 00:31:23 +0800

    f

---
+++ Golden_Top/DE10_LITE_Golden_Top.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 21.1.0 Build 842 10/21/2021 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484) Path("C:/Users/linmo/Desktop/Golden_Top/") File("DE10_LITE_Golden_Top.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ Golden_Top/software/hw_sw/obj/default/DE_hello_world.d
@@ -0,0 +1,13 @@
+obj/default/DE_hello_world.o: DE_hello_world.c \
+ c:\users\linmo\desktop\golden_top\software\hw_sw_bsp\hal\inc\sys\alt_stdio.h \
+ c:\users\linmo\desktop\golden_top\software\hw_sw_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ c:\users\linmo\desktop\golden_top\software\hw_sw_bsp\hal\inc\io.h \
+ c:\users\linmo\desktop\golden_top\software\hw_sw_bsp\hal\inc\alt_types.h \
+ c:/Users/linmo/Desktop/Golden_Top/software/hw_sw_bsp/system.h \
+ c:/Users/linmo/Desktop/Golden_Top/software/hw_sw_bsp/linker.h
+c:\users\linmo\desktop\golden_top\software\hw_sw_bsp\hal\inc\sys\alt_stdio.h:
+c:\users\linmo\desktop\golden_top\software\hw_sw_bsp\drivers\inc\altera_avalon_pio_regs.h:
+c:\users\linmo\desktop\golden_top\software\hw_sw_bsp\hal\inc\io.h:
+c:\users\linmo\desktop\golden_top\software\hw_sw_bsp\hal\inc\alt_types.h:
+c:/Users/linmo/Desktop/Golden_Top/software/hw_sw_bsp/system.h:
+c:/Users/linmo/Desktop/Golden_Top/software/hw_sw_bsp/linker.h:
